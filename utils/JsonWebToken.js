fb1d6b7ad2de7b96c33ef9b16793f9a9b8161919
Pedro Mançano <pedromancano@gmail.com>
2022-08-23 23:52:24 -0300

    hot fix (remove fs read file)

--- utils/JsonWebToken.js
+++ utils/JsonWebToken.js
@@ -1,10 +1,4 @@
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const path = require('path');
-const keys = {
-    privateKey: fs.readFileSync(path.join(__dirname, '..', 'keys', 'private.key')),
-    publicKey: fs.readFileSync(path.join(__dirname, '..', 'keys', 'public.key.pub')),
-}
 
 function sign(payload, options) {
     return jwt.sign(payload, process.env.JWT_SECRET, {
