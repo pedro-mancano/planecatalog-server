9ff3bf08f1f75edcc5e0ea4ba369b0ed8b0cc2eb
Pedro Mançano <pedromancano@gmail.com>
2022-08-25 00:36:35 -0300

    added filter support

--- routes/plane.route.js
+++ routes/plane.route.js
@@ -2,17 +2,37 @@ const router = require('express').Router();
 const Validator = require('../middleware/validation.middleware');
 const PlaneModel = require('../models/plane.model');
 
+
+function filterParse(filter) {
+    switch (filter.type) {
+        case 'number_range':
+            return { $gte: parseFloat(filter.value.min), $lte: parseFloat(filter.value.max) };
+        case 'string':
+            return { $regex: `${filter.value}`.toLocaleLowerCase(), $options: 'i' };
+    }
+}
+
 router.post('/query', Validator("PlaneQueryValidation"), async (req, res) => {
     const filter = req.body.filter;
 
-    //const plane = new PlaneModel({
-    //    name: filter,
-    //});
-    //
-    //plane.save();
+    const query = {};
+    if (filter.length > 0) {
+        filter.forEach(f => {
+            query[f.field] = filterParse(f);
+        });
+    }
 
-    const planes = await PlaneModel.find();
-    res.json(planes);
+    const planes = await PlaneModel.find(query).limit(20).skip(0).lean();
+
+    const cleanPlanes = planes.map(plane => {
+        delete plane._id;
+        delete plane.__v;
+        delete plane.createdAt;
+        delete plane.updatedAt;
+        return plane;
+    });
+
+    res.json(cleanPlanes);
 });
 
 module.exports = router;
--- validation/plane.validation.js
+++ validation/plane.validation.js
@@ -1,7 +1,16 @@
 const Joi = require('joi');
+const planeParams = require('../assets/planeParameters.json').parameters;
+const planeParamsNames = planeParams.map(param => param.name);
 
 const PlaneQueryValidation = Joi.object({
-    filter: Joi.string().email().required(),
+    filter: Joi.array().items(
+        Joi.object({
+            field: Joi.string().required().valid(...planeParamsNames),
+            type: Joi.string().required(),
+            value: Joi.object().required()
+        }).optional()
+    ).min(0),
+    skip: Joi.number().integer().min(0).default(0),
 }).options({
     stripUnknown: true,
     messages: {
